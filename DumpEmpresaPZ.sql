d057d899e24b2ba2c15d55edce5d327268d7640f
Luiz Gustavo De Almeida Lopes <gustavw1001@gmail.com>
2025-05-26 15:26:23 -0300

    🎞️ Mudança Banco de Dados
    Mudança em Alguns campos e seus atributos

--- DumpEmpresaPZ.sql
+++ DumpEmpresaPZ.sql
@@ -64,9 +64,10 @@ CREATE TABLE `employee` (
   `entryDate` date NOT NULL,
   `areaEmpl` varchar(30) NOT NULL,
   `emplPassword` varchar(100) NOT NULL,
+  `profilePicPath` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`idEmpl`),
   UNIQUE KEY `emailEmpl` (`emailEmpl`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -75,7 +76,7 @@ CREATE TABLE `employee` (
 
 LOCK TABLES `employee` WRITE;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
-INSERT INTO `employee` VALUES (1,'Farofilson Bananilson','2001-06-10','bananilson@gmail.com','M','(31) 9 7505 4252','Recursos Humanos','2018-01-11','Gerencia','$2y$10$40jqOXWnjVJOgf0cgARSEOFfp3OAhgAF.xsvFE8Nq2wNd135pJ98O'),(2,'Clara Fenizza','1979-11-18','fenilson10@gmail.com','F','(33) 9 4319 9873','Gerencia','2009-03-19','Projetos','$2y$10$9QDBZOjGnnKIf7vAQUKWtOymnBAMsHFlNdEiVuZARPXem/8uPlroi'),(3,'Ademilson Tupilson','1990-10-11','ademilson@gmail.com','M','(35) 9 4533 9034','Operacoes','2020-02-18','Projetos','$2y$10$s5K8c8hDQ1sZGx3K1JQZWes7qaDk71vNfQQ0G1HMGHtuPSlv8dTMG'),(4,'Ana Banana','2006-05-15','anaBnn@gmail.com','F','(31) 9 8779 4255','Operacoes','2024-10-24','Projetos','$2y$10$zFDotE8yLBYWTDNhSKcHZelN6te9H7NDN5o3iWeC7OY56Wx59/v6e');
+INSERT INTO `employee` VALUES (1,'Farofilson Bananilson','2001-06-05','bananilson4324@gmail.com','M','(31) 9 7450 5802','Infraestrutura','2018-01-11','Projetos','$2y$10$40jqOXWnjVJOgf0cgARSEOFfp3OAhgAF.xsvFE8Nq2wNd135pJ98O','../users-page/user-images/img_6830da8f0764d.jpg'),(2,'Clara Fenizza','1979-11-18','fenilson10@gmail.com','F','(33) 9 4319 9873','Gerencia','2009-03-19','Projetos','$2y$10$9QDBZOjGnnKIf7vAQUKWtOymnBAMsHFlNdEiVuZARPXem/8uPlroi','../users-page/user-images/img_682bab5f60d5b.jpg'),(3,'Ademilson Tupilson','1990-10-11','ademilson9656@gmail.com','M','(35) 9 4533 9034','Operacoes','2020-02-18','Projetos','$2y$10$s5K8c8hDQ1sZGx3K1JQZWes7qaDk71vNfQQ0G1HMGHtuPSlv8dTMG','../users-page/user-images/img_682bab5f60d5c.jpg'),(4,'Ana Banana','2006-05-15','anaBnn8756@gmail.com ','F','(31) 9 8779 4255','Operacoes','2024-10-24','Projetos','$2y$10$zFDotE8yLBYWTDNhSKcHZelN6te9H7NDN5o3iWeC7OY56Wx59/v6e','../users-page/user-images/img_682bab5f60d5d.png');
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -89,12 +90,12 @@ DROP TABLE IF EXISTS `rescuepassword`;
 CREATE TABLE `rescuepassword` (
   `idRescue` int(11) NOT NULL AUTO_INCREMENT,
   `rescueToken` varchar(6) NOT NULL,
-  `dayLimit` varchar(2) NOT NULL,
+  `dayLimit` varchar(10) NOT NULL,
   `hourLimit` varchar(5) NOT NULL,
   `emailReciever` varchar(40) NOT NULL,
   PRIMARY KEY (`idRescue`),
   UNIQUE KEY `rescueToken` (`rescueToken`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,6 +104,7 @@ CREATE TABLE `rescuepassword` (
 
 LOCK TABLES `rescuepassword` WRITE;
 /*!40000 ALTER TABLE `rescuepassword` DISABLE KEYS */;
+INSERT INTO `rescuepassword` VALUES (47,'029d44','2025-05-22','00:15','anaBnn@gmail.com');
 /*!40000 ALTER TABLE `rescuepassword` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -119,4 +121,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-18 19:38:34
+-- Dump completed on 2025-05-26 15:25:07
